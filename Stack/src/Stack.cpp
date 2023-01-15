6141c4be1fada53b34243326f1b4842b46b2fa50
Brandon Werner <brandonclayw@gmail.com>
2023-01-14 21:26:30 -0500

    Feat: Added stack data strucutre boilerplate

---
+++ Stack/src/Stack.cpp
@@ -0,0 +1,16 @@
+#include "../../LinkedList/src/List.hpp"
+
+template <typename T, class Container = List<T>>
+class Stack
+{
+public:
+	Stack() {}
+	~Stack() {}
+
+	bool empty() {}
+	size_t Size() {}
+	T top() {}
+	void push() {}
+	void emplace() {}
+	void pop() {}
+};
