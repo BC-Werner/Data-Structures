eb2edf64553db3dedc1dd46cffcd96564f86abd7
Brandon Werner <brandonclayw@gmail.com>
2023-01-24 13:30:30 -0500

    Fix: updated move constructor and swap

--- Stack/src/Stack.hpp
+++ Stack/src/Stack.hpp
@@ -1,15 +1,16 @@
 #pragma once
 #include "../../LinkedList/src/List.hpp"
+#include <initializer_list>
 
 template <typename T, class Container = List<T>>
 class Stack
 {
 public:
 	Stack() : count(0) {}
-	Stack(Stack<T, Container>&& other)
+	Stack(const std::initializer_list<T>& il) : count(il.size()), container(il) {}
+	Stack(Stack<T, Container>&& other) noexcept : count(0), container() 
 	{
-		container = std::move(other.container);
-		count = other.count;
+		other.swap(*this);
 	}
 	~Stack() { clear(); }
 
@@ -20,34 +21,10 @@ public:
 	void push(T data) { container.push_front(data); count++; }
 	void pop() { container.pop_front(); count--; }
 
-	void swap(Stack<T, Container>& other) 
+	void swap(Stack<T, Container>& other) noexcept
 	{
-		Stack<T, Container> tmp1;
-		Stack<T, Container> tmp2;
-
-		while (!other.empty())
-		{
-			tmp1.push(other.top());
-			other.pop();
-		}
-
-		while (!empty())
-		{
-			tmp2.push(top());
-			pop();
-		}
-
-		while (!tmp1.empty())
-		{
-			push(tmp1.top());
-			tmp1.pop();
-		}
-
-		while (!tmp2.empty())
-		{
-			other.push(tmp2.top());
-			tmp2.pop();
-		}
+		std::swap(count, other.count);
+		container.swap(other.container);
 	}
 
 	void clear() { container.clear(); count = 0; }
