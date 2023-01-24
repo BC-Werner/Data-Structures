f35f9ca3e7be4821862c404095701317330d497d
Brandon Werner <brandonclayw@gmail.com>
2023-01-24 15:15:05 -0500

    Feat: added tests for stack class

--- Tests/StackTests/StackTests.cpp
+++ Tests/StackTests/StackTests.cpp
@@ -9,8 +9,122 @@ namespace StackTests
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(Default_Constructor)
 		{
+			Stack<int> stack;
+
+			Assert::IsTrue(stack.empty());
+		}
+		
+		TEST_METHOD(Copy_Constructor)
+		{
+			Stack<int> stack;
+			stack.push(1);
+			stack.push(2);
+			stack.push(3);
+
+			Stack<int> copy(stack);
+
+			Assert::IsFalse(stack.empty());
+			Assert::IsTrue(stack.size() == copy.size());
+
+			while (!stack.empty())
+			{
+				Assert::AreEqual(stack.top(), copy.top());
+				stack.pop();
+				copy.pop();
+			}
+		}
+		
+		TEST_METHOD(InitializerList_Constructor)
+		{
+			Stack<int> stack{ 1, 2, 3, 4, 5 };
+
+			Assert::IsFalse(stack.empty());
+		}
+		
+		TEST_METHOD(Top_EmptyStack_ShouldThrow)
+		{
+			Assert::ExpectException<std::exception>([]() {
+				Stack<int> stack;
+				stack.top();
+			});
+		}
+		
+		TEST_METHOD(Top_ShouldReturnFirstOnTheStack)
+		{
+			Stack<int> stack{ 1, 2, 3, 4, 5 };
+
+			Assert::IsFalse(stack.empty());
+			Assert::IsTrue(stack.top() == 1);
+		}
+		
+		TEST_METHOD(Pop_EmptyStack)
+		{
+			Stack<int> stack;
+			stack.pop();
+
+			Assert::IsTrue(stack.empty());
+			Assert::IsTrue(stack.size() == 0);
+		}
+		
+		TEST_METHOD(Pop_ShouldRemoveFirstOnStack)
+		{
+			Stack<int> expected{ 2, 3, 4, 5 };
+			Stack<int> actual{ 1, 2, 3, 4, 5 };
+
+			Assert::IsFalse(actual.empty());
+			Assert::IsTrue(actual.top() == 1);
+			actual.pop();
+			Assert::IsTrue(actual.top() == 2);
+			Assert::IsTrue(actual.size() == 4);
+
+			Assert::IsTrue(actual.size() == expected.size());
+
+			while (!actual.empty())
+			{
+				Assert::IsTrue(expected.top() == actual.top());
+				expected.pop();
+				actual.pop();
+			}
+		}
+		
+		TEST_METHOD(Pop_EntireStack_ShouldBeEmpty)
+		{
+			Assert::ExpectException<std::exception>([]() {
+				Stack<int> actual{ 1, 2, 3, 4, 5 };
+
+				Assert::IsFalse(actual.empty());
+				while (!actual.empty())
+					actual.pop();
+
+				Assert::IsTrue(actual.empty());
+
+				actual.top();
+			});
+		}
+
+		TEST_METHOD(Push_EmptyStack_ShouldHaveOneElement)
+		{
+			Stack<int> stack;
+			stack.push(1);
+
+			Assert::IsFalse(stack.empty());
+			Assert::IsTrue(stack.size() == 1);
+			Assert::IsTrue(stack.top() == 1);
+		}
+
+		TEST_METHOD(Push_EmptyStack_ShouldHaveTwoElements)
+		{
+			Stack<int> stack;
+			stack.push(1);
+			stack.push(2);
+
+			Assert::IsFalse(stack.empty());
+			Assert::IsTrue(stack.size() == 2);
+			Assert::IsTrue(stack.top() == 2);
+			stack.pop();
+			Assert::IsTrue(stack.top() == 1);
 		}
 	};
 }
