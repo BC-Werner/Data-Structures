c5135cf3b91f5f5d3efb097ac09f5c63bac03708
Brandon Werner <brandonclayw@gmail.com>
2023-01-24 15:50:38 -0500

    Feat: added queue tests

---
+++ Tests/QueueTests/QueueTests.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace QueueTests
+{
+	TEST_CLASS(QueueTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
