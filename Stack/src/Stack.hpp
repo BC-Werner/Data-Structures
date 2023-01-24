983e95eaa89e8f2f094c43b8bb7151d3c277c276
Brandon Werner <brandonclayw@gmail.com>
2023-01-24 15:14:52 -0500

    Fix: refactor and added copy constructor

--- Stack/src/Stack.hpp
+++ Stack/src/Stack.hpp
@@ -7,27 +7,23 @@ class Stack
 {
 public:
 	Stack() : count(0) {}
+	Stack(const Stack<T>& other) : count(other.count), container(other.container) {}
 	Stack(const std::initializer_list<T>& il) : count(il.size()), container(il) {}
-	Stack(Stack<T, Container>&& other) noexcept : count(0), container() 
-	{
-		other.swap(*this);
-	}
+	Stack(Stack<T, Container>&& other) noexcept : count(0), container() { other.swap(*this); }
 	~Stack() { clear(); }
 
-	const bool empty() const { return count == 0; }
-	const size_t Size() const { return count; }
-
-	T& top() { return container.front(); }
-	void push(T data) { container.push_front(data); count++; }
-	void pop() { container.pop_front(); count--; }
+	const bool		empty() const	{ return count == 0; }
+	const size_t	size() const	{ return count; }
+	T&				top()			{ return container.front(); }
+	void			push(T data)	{ container.push_front(data); count = container.size(); }
+	void			pop()			{ if (!empty()) { container.pop_front(); count = container.size(); } }
+	void			clear()			{ container.clear(); count = container.size(); }
 
 	void swap(Stack<T, Container>& other) noexcept
 	{
 		std::swap(count, other.count);
 		container.swap(other.container);
 	}
-
-	void clear() { container.clear(); count = 0; }
 
 private:
 	size_t count;
