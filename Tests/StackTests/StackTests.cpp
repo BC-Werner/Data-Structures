fafc7515b88bc80dc833e45818ee18e61ce4731c
Brandon Werner <brandonclayw@gmail.com>
2023-01-24 13:50:08 -0500

    Added Stack unit test project

---
+++ Tests/StackTests/StackTests.cpp
@@ -0,0 +1,16 @@
+#include "CppUnitTest.h"
+#include "../../Stack/src/Stack.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace StackTests
+{
+	TEST_CLASS(StackTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
