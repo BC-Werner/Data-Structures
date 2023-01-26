9e766e8bb5c52f70dabfc9ee877505f2bde6111b
Brandon Werner <brandonclayw@gmail.com>
2023-01-26 11:50:48 -0500

    Feat: added basic queue functionality

--- Queue/src/Queue.hpp
+++ Queue/src/Queue.hpp
@@ -9,9 +9,17 @@ public:
 	Queue() : m_size(0) {}
 	Queue(const Queue<T, Container>& other) : m_size(other.m_size), container(other.container) {}
 	Queue(const std::initializer_list<T>& il) : m_size(il.size()), container(il) {}
-	Queue(Queue<T, Container>&& other) : m_size(0), container() { other.swap(*this); }
+	Queue(Queue<T, Container>&& other) noexcept : m_size(0), container() { other.swap(*this); }
+	~Queue() { clear(); }
 
-	void swap(Queue<T, Container>& other)
+	const bool		empty() const		{ return m_size == 0; }
+	const size_t	size() const		{ return m_size; }
+	T&				top() const			{ return container.front(); }
+	void			push(const T& data) { container.push_back(data); }
+	void			pop()				{ container.pop_front(); }
+	void			clear()				{ container.clear(); count = container.size(); }
+
+	void swap(Queue<T, Container>& other) noexcept
 	{
 		std::swap(m_size, other.m_size);
 		container.swap(other.container);
