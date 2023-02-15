867e8a94288b306037dea598de08772f0f940a43
Brandon Werner <brandonclayw@gmail.com>
2023-02-15 12:11:58 -0500

    Feat: Added binary tree data structure

---
+++ Tests/BinaryTreeTests/BinaryTreeTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BinaryTreeTests
+{
+	TEST_CLASS(BinaryTreeTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
