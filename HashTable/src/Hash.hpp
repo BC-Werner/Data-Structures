5e23e003e860a91c9efa0c43d96df47fe20e8bee
Brandon Werner <brandonclayw@gmail.com>
2023-06-22 12:07:42 -0400

    Feat: added template specialization for string hash function

--- HashTable/src/Hash.hpp
+++ HashTable/src/Hash.hpp
@@ -6,3 +6,15 @@ class Hash
 public:
 	size_t operator () (const K& key) { return static_cast<size_t>(key); }
 };
+
+template <>
+class Hash<std::string>
+{
+	size_t operator () (const std::string& key)
+	{
+		size_t x = 0;
+		for (char c : key)
+			x += c;
+		return x;
+	}
+};
